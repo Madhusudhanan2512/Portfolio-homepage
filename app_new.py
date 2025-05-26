ff03ac386af97169c205718d8691e6e59494ff77
Madhusudhanan2512 <131872189+Madhusudhanan2512@users.noreply.github.com>
2025-05-26 15:44:57 +0530

    Update app_new.py

--- app_new.py
+++ app_new.py
@@ -206,6 +206,18 @@ elif sidebar_choice == "Virtual Internships":
     """)
     st.markdown("[Open App](https://your-pwc-app-link)  <!-- Add target='_blank' in HTML if you wish -->", unsafe_allow_html=True)
 
+    st.subheader("BCG Data Science Job Simulation")
+    st.write(""" 
+    -**Skills:** Business Understanding, Client Communication, Hypothesis Framing, Data Visualization.
+
+    **Project Overview:**
+    - Completed a customer churn analysis simulation for XYZ Analytics, demonstrating advanced data analytics skills, identifying essential client data and outlining a strategic investigation approach.
+    - Conducted efficient data analysis using Python, including Pandas and NumPy. Employed data visualization techniques for insightful trend interpretation.
+    - Completed the engineering and optimization of a random forest model, achieving an 85% accuracy rate in predicting customer churn.
+    - Completed a concise executive summary for the Associate Director, delivering actionable insights for informed decision-making based on the analysis.
+    """)
+    st.markdown("[Open App](https://your-pwc-app-link)  <!-- Add target='_blank' in HTML if you wish -->", unsafe_allow_html=True)
+
     st.subheader("Standard Bank Data Science Simulation")
     st.write(""" 
     
